184e5a6f9268b439c68cf89a2ef97e75136f6869
Dark2Knight7 <ducpeti27@gmail.com>
2023-03-03 13:29:47 +0100

    Sárkányos feladat létrehozása és megoldása.

---
+++ SzamolosFeladatok/src/ciklusok/Sarkanyok.java
@@ -0,0 +1,19 @@
+package ciklusok;
+
+public class Sarkanyok {
+    
+    public static void main(String[] args) {
+        
+        for (int x = 1; x < 20; x++) {
+            for (int y = 1; y < 13; y++) {
+                if (7*x + 11*y == 145) {
+                    System.out.println("7db fejű: " + x);
+                    System.out.println("11db fejű: " + y);
+                    System.out.println("------------");
+                }
+            }
+        }
+        
+    }
+    
+}
--- SzamolosFeladatok/src/ciklusok/Szamok.java
+++ SzamolosFeladatok/src/ciklusok/Szamok.java
@@ -1,14 +1,37 @@
 package ciklusok;
 
+import java.util.Scanner;
+
 public class Szamok {
+    
+    private static Scanner sc = new Scanner(System.in);
+    
     public static void main(String[] args) {
         
+        System.out.print("Ismétléssel? (i/n): ");
+        String v = sc.next();
+        boolean ismetel = v.equals("i");
+        
+        int ismDb = 0;
+        int ismNlkDb = 0;
         for (int k = 2; k <= 4; k++) {
             for (int j = 2; j <= 3; j++) {
                 for (int i = 2; i <= 4; i++) {
-                    System.out.println(""+ k + j + i);
+                    if (ismetel) {
+                        System.out.printf("%d%d%d\n", k, j, i);
+                        ismDb++;
+                    }else if (k != j && k != i && i != j) {
+                        System.out.printf("%d%d%d\n", k, j, i);
+                        ismNlkDb++;
+                    }
                 }
             }
+        }
+        
+        if (ismetel) {
+            System.out.println("ismétléssel: " + ismDb);
+        }else{
+            System.out.println("ismétlés nélkül" + ismNlkDb);
         }
         
     }
